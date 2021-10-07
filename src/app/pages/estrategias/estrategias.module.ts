66eb752d0043e244a37994f313d6928746c84059
Heydi Perez <hperezn@tesla.cujae.edu.cu>
2021-10-07 13:09:15 +0200

    estrategias

--- src/app/pages/estrategias/estrategias.module.ts
+++ src/app/pages/estrategias/estrategias.module.ts
@@ -4,6 +4,12 @@ import { CommonModule } from '@angular/common';
 import { EstrategiasRoutingModule } from './estrategias-routing.module';
 import { EstrategiasComponent } from './estrategias.component';
 import { MatTableModule } from '@angular/material/table'; 
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button'; 
+
+
+
+
 
 
 @NgModule({
@@ -13,7 +19,9 @@ import { MatTableModule } from '@angular/material/table';
   imports: [
     CommonModule,
     EstrategiasRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule
   ]
 })
 export class EstrategiasModule { }
