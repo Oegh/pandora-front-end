16b5990c01ee7a743f5643684628281510281f9c
HeydiPerez97 <heydip806@gmail.com>
2022-05-24 20:07:33 -0400

    ModuleHome

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,16 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/core/services/auth.service';
 
+interface Facultad {
+  value: string;
+  viewValue: string;
+}
+
+export interface PeriodicElement {
+  name: string;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { name: 'Heydi', weight: 97100408555, symbol: 'DISERTIC'},
+  { name: 'Layday', weight: 97100408555, symbol: 'DISERTIC'},
+  { name: 'Oscar', weight: 97100408555, symbol: 'DISERTIC'},
+  { name: 'Dianette', weight: 97100408555, symbol: 'DISERTIC'},
+  { name: 'Carlos', weight: 97100408555, symbol: 'DISERTIC'},
+];
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  displayedColumns: string[] = ['name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
 
   constructor(public authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  facultades: Facultad[] = [
+    {value: 'steak-0', viewValue: 'Facultad de Ingeniería Informática'},
+    {value: 'pizza-1', viewValue: 'Facultad de Ingeniería Civil'},
+    {value: 'tacos-2', viewValue: 'Facultad de Ingeniería Mecánica'},
+  ];
+
+  departamentos: Facultad[] = [
+    {value: 'steak-0', viewValue: 'Departamento de Ingeniería de Software'},
+    {value: 'pizza-1', viewValue: 'Departamento de Inteligencia Artificial'},
+    {value: 'tacos-2', viewValue: 'Departamento de Bases de Datos'},
+  ];
+
 }
+
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -4,6 +4,16 @@ import { CommonModule } from '@angular/common';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatDividerModule} from '@angular/material/divider';
 
 @NgModule({
   declarations: [
@@ -11,7 +21,18 @@ import { HomeComponent } from './home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatTableModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatCardModule,
+    MatProgressBarModule,
+    MatDividerModule,  
+    MatIconModule,
   ]
 })
 export class HomeModule { }
