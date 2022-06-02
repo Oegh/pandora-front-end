3315946b20a293d2df126efc27db20d93c454ae8
Heydi Perez <hperezn@tesla.cujae.edu.cu>
2022-06-02 06:12:32 -0400

    Carga docente del departamento module and component

---
+++ src/app/pages/carga-docente-departamento/carga-docente-departamento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-carga-docente-departamento',
+  templateUrl: './carga-docente-departamento.component.html',
+  styleUrls: ['./carga-docente-departamento.component.css']
+})
+export class CargaDocenteDepartamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
