dff1380594196f1f822b09828a48799ff3d863bd
HeydiPerez97 <heydip806@gmail.com>
2022-06-08 19:28:27 -0400

    Todos los componentes

--- src/app/pages/cargadepartamento/cargadepartamento.component.ts
+++ src/app/pages/cargadepartamento/cargadepartamento.component.ts
@@ -1,9 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import {Sort} from '@angular/material/sort';
+
+export interface Dessert {
+  calories: number;
+  carbs: number;
+  fat: number;
+  name: string;
+  protein: number;
+}
 
 interface Food {
   value: string;
   viewValue: string;
 }
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-cargadepartamento',
@@ -11,8 +40,20 @@ interface Food {
   styleUrls: ['./cargadepartamento.component.css']
 })
 export class CargadepartamentoComponent implements OnInit {
+  desserts: Dessert[] = [
+    {name: 'Frozen yogurt', calories: 159, fat: 6, carbs: 24, protein: 4},
+    {name: 'Ice cream sandwich', calories: 237, fat: 9, carbs: 37, protein: 4},
+    {name: 'Eclair', calories: 262, fat: 16, carbs: 24, protein: 6},
+    {name: 'Cupcake', calories: 305, fat: 4, carbs: 67, protein: 4},
+    {name: 'Gingerbread', calories: 356, fat: 16, carbs: 49, protein: 4},
+  ];
 
-  constructor() { }
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  dataSource = ELEMENT_DATA;
+    
+  sortedData: Dessert[];
+
+  constructor() { this.sortedData = this.desserts.slice();}
 
   ngOnInit(): void {
   }
@@ -32,5 +73,35 @@ export class CargadepartamentoComponent implements OnInit {
     { value: 'pizza-1', viewValue: '2do Período' },
     { value: 'pizza-1', viewValue: '3er Período' },
   ];
+
+  sortData(sort: Sort) {
+    const data = this.desserts.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a, b) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'name':
+          return compare(a.name, b.name, isAsc);
+        case 'calories':
+          return compare(a.calories, b.calories, isAsc);
+        case 'fat':
+          return compare(a.fat, b.fat, isAsc);
+        case 'carbs':
+          return compare(a.carbs, b.carbs, isAsc);
+        case 'protein':
+          return compare(a.protein, b.protein, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 
 }
--- src/app/pages/cargadepartamento/cargadepartamento.module.ts
+++ src/app/pages/cargadepartamento/cargadepartamento.module.ts
@@ -6,6 +6,9 @@ import { CargadepartamentoComponent } from './cargadepartamento.component';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSortModule} from '@angular/material/sort';
+import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
   declarations: [
@@ -17,6 +20,9 @@ import {MatSelectModule} from '@angular/material/select';
     MatDividerModule,
     MatCardModule,
     MatSelectModule,
+    MatButtonModule,
+    MatSortModule,
+    MatTableModule,
   ]
 })
 export class CargadepartamentoModule { }
